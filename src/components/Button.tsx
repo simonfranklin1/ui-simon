160b65c06df4130c0dc7ce35748e2fb3f3a1c964
Simon Franklin <simonfranklin.54@gmail.com>
2024-06-10 13:26:19 -0300

    refactor: CustomButton

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,19 +6,19 @@ interface ButtonI {
     display: 'block' | 'flex' | 'inline-block' | 'none',
     onClick?: () => void,
     background: string,
-    textColor: string | "#000",
-    borderRadius?: string | "0px",
+    textColor: "#000" | string,
+    borderRadius?: "0px" | string,
     text: string,
     link?: {
-        where: string,
-        target?: "default" | "_blank";
+        where?: string,
+        target?: "_blank" | "_self" | "_parent" | "_top";
     },
     hoverBackground?: string,
     animation?: "grow" | "up" | "down",
     cursor?: "moz-initial" | "pointer" | "all" | "cell" | "copy",
-    duration?: string | "initial",
-    transition?: string | "ease" | "ease-in-out" | "ease-in",
-    border: "none" | string
+    duration?: string | "0s",
+    transition?: "moz-initial" | "ease" | "ease-in-out" | "ease-in",
+    border?: string | "none"
 }
 
 const Button = ({
@@ -41,37 +41,38 @@ const Button = ({
         <>
             {
                 link !== null && (
-                    <a href={link?.where} target={link?.target}>
-                        <Box
-                            display={display}
-                            padding={padding}
-                            onClick={onClick && onClick}
-                            bgcolor={background}
-                            color={textColor}
-                            borderRadius={borderRadius}
-                            border={border}
-                            sx={{
-                                transitionDuration: duration,
-                                transition: transition,
-                                cursor: cursor,
-                                "&:hover": animation === "grow" ?
+                    <Box
+                        display={display}
+                        padding={padding}
+                        onClick={onClick && onClick}
+                        bgcolor={background}
+                        color={textColor}
+                        borderRadius={borderRadius}
+                        border={border}
+                        component={"a"}
+                        href={link?.where}
+                        target={link?.target}
+                        sx={{
+                            transitionDuration: duration,
+                            transition: transition,
+                            cursor: cursor,
+                            "&:hover": animation === "grow" ?
+                                {
+                                    scale: "1.1", background: hoverBackground || null
+                                } :
+                                animation === "up" ?
                                     {
-                                        scale: "1.1", background: hoverBackground || null
+                                        transform: "translateY(-0.25rem)",
+                                        background: hoverBackground || null
                                     } :
-                                    animation === "up" ?
-                                        {
-                                            transform: "translateY(-0.25rem)",
-                                            background: hoverBackground || null
-                                        } :
-                                        animation === "down" ? {
-                                            transform: "translateY(0.25rem)",
-                                            background: hoverBackground || null
-                                        } : null
-                            }}
-                        >
-                            {text}
-                        </Box>
-                    </a>
+                                    animation === "down" ? {
+                                        transform: "translateY(0.25rem)",
+                                        background: hoverBackground || null
+                                    } : null
+                        }}
+                    >
+                        {text}
+                    </Box>
                 ) || (
                     <Box
                         display={display}
@@ -82,8 +83,7 @@ const Button = ({
                         borderRadius={borderRadius}
                         border={border}
                         sx={{
-                            transitionDuration: duration,
-                            transition: transition,
+                            transition: `${duration} ${duration}`,
                             cursor: cursor,
                             "&:hover": animation === "grow" ?
                                 {
